95df11d0eb90596d12add607899da4d1e6099ea7
Ryan Coote <rcoote@villanova.edu>
2024-11-17 11:25:19 -0500

    Project Stand Up
    Standing Up the Project. Basic Web Page

---
+++ src/main/java/com/example/Grcer/GrcerApplication.java
@@ -0,0 +1,11 @@
+package com.example.Grcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrcerApplication {
+
+	public static void main(String[] args) {SpringApplication.run(GrcerApplication.class, args); }
+
+}
---
+++ src/test/java/com/example/Grcer/GrcerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grcer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrcerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
